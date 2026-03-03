f69afd4ab687f5a36d0be46992317c41858a71e9
DominikGrom <40479@student.atar.edu.pl>
2026-03-03 12:22:29 +0100

    Create test.kt

---
+++ test.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var pi: Double = 3.14
+    var r = 5
+
+    var poleK  = pi + (r*r)
+    println("Pole kola = ${poleK}")
+
+    var a: Double = 5.0
+    var b: Double = 5.0
+    var c: Double = 4.0
+
+    var srednia: Double = (a+b+c)/3
+    println("Srednia = $srednia")
+
+    var totalSeconds: Int = 86450
+    var hours = totalSeconds / 3600
+    var minutes = totalSeconds / 60
+
+    println("Hours = $hours")
+    println("Minutes = $minutes")
+}
