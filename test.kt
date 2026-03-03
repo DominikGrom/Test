77309603fff801282b805c8a72ed1ffe7b67bcc5
DominikGrom <40479@student.atar.edu.pl>
2026-03-03 12:31:55 +0100

    Update test.kt

--- test.kt
+++ test.kt
@@ -4,7 +4,8 @@ fun main() {
     var r = 5
 
     var poleK  = pi + (r*r)
-    println("Pole kola = ${poleK}")
+    println("Zadanie 1")
+    println("Pole kola = ${poleK} \n")
 
     //Zadanie 2
     var a: Double = 5.0
@@ -12,13 +13,15 @@ fun main() {
     var c: Double = 4.0
 
     var srednia: Double = (a+b+c)/3
-    println("Srednia = ${srednia}")
+    println("Zadanie 2")
+    println("Srednia = ${srednia} \n")
 
     //Zadanie 3
     var totalSeconds: Int = 86450
     var hours = totalSeconds / 3600
     var minutes = totalSeconds / 60
 
+    println("Zadanie 3")
     println("Hours = ${hours}")
     println("Minutes = ${minutes}")
 }
